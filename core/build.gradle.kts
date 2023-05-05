239870ed8ac4d3de2a48924000ce234160703831
andylamax <andylamax@programmer.net>
2023-05-05 05:05:43 +0300

    Atleast astrix is now attempting to build. Although it apears to be failing

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -28,7 +28,7 @@ kotlin {
 
         val commonTest by getting {
             dependencies {
-                api(projects.expectCore)
+                api(projects.kommanderCore)
             }
         }
 
